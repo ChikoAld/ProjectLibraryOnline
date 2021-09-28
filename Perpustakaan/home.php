2318bde17b2066d06cab4989907b12d91d0f2473
Ko <88018033+ChikoAld@users.noreply.github.com>
2021-09-28 11:08:17 +0700

    Add files via upload

---
+++ Perpustakaan/home.php
@@ -0,0 +1,12 @@
+<?php
+include "header.php";
+?>
+<div class="container-fluid" align="center">
+<br><br>
+        <img src="Pustel.png" height="200px" weight="200px"><br><br><br><br><br>
+        <h3>Halo <?=$_SESSION['nama_siswa']?>, Selamat datang di <b>Pustel</b> (Perpustakan SMK Telkom Malang)</h3>
+        <h5>"Apapun bidang yang anda tekuni, anda tetap harus membaca"</h5>
+    </div>
+<?php
+include "footer.php";
+?>
